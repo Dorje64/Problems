2831eb85667676d739783d2fa1b0a326dbdb31ae
Dorje <ku.dorjee@gmail.com>
2017-05-02 14:36:53 +0545

    generate csv file

--- Problem1/index.rb
+++ Problem1/index.rb
@@ -7,6 +7,16 @@ format_hash = {}
 group_data = csv.group_by { |data| data[1] }
 hash_data = Hash[group_data.map{|k,v| [k,v]}].sort
 
+def generate_file(file_name)
+  # File.truncat(file_name)
+  binding.pry
+  format_hash.each do |key ,value|
+    CSV.open(file_name, 'a+') do |csv_data|
+      csv_data << [value[:key] , value[:MaxTime], value[:volume], value[:WeightedAveragePrice], value[:MaxPrice]]
+    end
+  end
+end
+
 hash_data.each do |key, value|
 
   time_diff = []
@@ -20,15 +30,21 @@ hash_data.each do |key, value|
   volume = transpose_values[2].inject(0){|sum, e| sum + e.to_i}
   weightedPrice = value.inject(0){|total, e| (total + e[2].to_i * e[3].to_i)}
 
-  CSV.open('output.csv', 'a+') do |csv_data|
-    csv_data << [ key , time_diff.max, volume, weightedPrice/volume, transpose_values.last.max]
-  end
+  format_hash[key] = {
+                      symbol: key,
+                      MaxTime: time_diff.max,
+                      volume: volume,
+                      WeightedAveragePrice: weightedPrice/volume,
+                      MaxPrice: transpose_values.last.max
+                     }
+end
 
-  # format_hash[key] = {
-  #                     symbol: key,
-  #                     MaxTime: time_diff.max,
-  #                     volume: volume,
-  #                     WeightedAveragePrice: weightedPrice/volume,
-  #                     MaxPrice: transpose_values.last.max
-  #                    }
+
+
+unless File.exists?('output.csv')
+  generate_file('output.csv')
+else
+  puts "output.csv is already there in the directory"
+  to_replace = gets
+  generate_file('output.csv') if to_replace == 'y'
 end
